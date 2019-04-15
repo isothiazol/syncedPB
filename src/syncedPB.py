12d5852d547934144a2e5c1b2ed0e70c0814f1ca
ERASJAV <erasjav@emb-l3jajgh5.ki.sw.ericsson.se>
2019-04-15 15:38:11 +0200

    1st version

---
+++ src/syncedPB.py
@@ -0,0 +1,78 @@
+from time import time as timmer
+import sys
+import os
+from IPython.display import display, Javascript
+
+def _genStr(it, total, eta, length_bar = 20):
+    outstr = str(it)+'/'+str(total)+'  ['
+    perc_it = it/total * length_bar
+
+    for i in range(length_bar):
+        if i < perc_it:
+            outstr = outstr + chr(9608)
+        else:
+            outstr = outstr + ' '
+    outstr = outstr + '] \t\t '
+
+    min_eta = round(eta//60)
+    sec_eta = round(eta - 60 * min_eta)
+
+    if min_eta > 0:
+        outstr = outstr + str(min_eta)+' min \t'+str(sec_eta)+' sec'
+    else:
+        outstr = outstr + str(sec_eta)+' sec'
+        
+    outstr = outstr.ljust(100)
+
+    return outstr
+    
+def _ema(x, mu, alpha=0.3):
+    return (alpha * x) + (1 - alpha) * mu
+
+def foo():
+    
+    js = """
+
+    require(
+        ["base/js/dialog"], 
+        function(dialog) {
+            dialog.modal({
+                title: 'Code completed !',
+                body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pretium nulla at nisl hendrerit, sit amet facilisis erat ultrices. Integer ac mauris eget sem egestas tempus sit amet a neque. In nec gravida arcu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque non velit eu lectus feugiat gravida. Duis vitae dui non quam bibendum bibendum eget eu orci. Sed eget dui sed sapien mattis tempus id in nisl. Duis sagittis lectus ante. Praesent id imperdiet ipsum. Ut nec fermentum ante. Donec nec tortor lorem. Quisque condimentum sapien eget lacus varius, id mattis mauris molestie. Curabitur pharetra feugiat sem, eget faucibus tortor rutrum nec.',
+            });
+        }
+    );
+
+    """
+
+    display(Javascript(js))
+
+class syncedPB():
+    def __init__(self, target, file=sys.stderr, popup=False):
+        self.__popup = popup
+        self.__file = file
+        self.__target = target
+        self.tick = timmer()
+        self.it = 0
+        self.time_it = 0
+    
+    def __len__(self):
+        return len(self.__target)
+
+    def __getitem__(self, i):
+        
+        if self.it > 0:
+            self.time_it = _ema(timmer() - self.tick, self.time_it)
+        else:
+            self.time_it = _ema(timmer() - self.tick, self.time_it, alpha=1)
+            
+        eta = int((len(self) - self.it)*self.time_it)
+        
+        print(_genStr(self.it, len(self), eta), file=self.__file, end='\r')
+        self.tick = timmer()
+        self.it += 1
+        
+        if i == len(self)-1 and self.__popup:
+            foo()
+        
+        return self.__target[i]
